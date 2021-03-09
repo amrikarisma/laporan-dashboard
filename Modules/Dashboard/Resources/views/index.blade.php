67656f11ba3b975383805cab7f5d00e089d17bde
Amri Karisma <amrikarisma@yahoo.com>
2021-03-10 01:47:11 +0700

    change name of app

--- Modules/Dashboard/Resources/views/index.blade.php
+++ Modules/Dashboard/Resources/views/index.blade.php
@@ -6,7 +6,7 @@
         <div class="card h-100">
             <div class="card-body">
                 <div style="display:flex; justify-content:center; align-self:center; align-items:center; flex-wrap: wrap;">
-                    <h1 style="text-align:center; margin-bottom: 40px">Selamat Datang di Sistem Informasi Pelaporan Kesatuan Umat dan Bangsa (SI KUMBANG)</h1>
+                    <h1 style="text-align:center; margin-bottom: 40px">Sistem Informasi & Komunikasi Kesatuan Bangsa (SIKUMBANG)</h1>
                     <div class="box-logo my-4">
                         <img class="img-fluid" src="{{asset('image/logo-sikumbang.png') }}" alt="Logo SiKumbang">
                     </div>
