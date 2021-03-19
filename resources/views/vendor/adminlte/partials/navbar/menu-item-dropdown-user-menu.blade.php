888b0f4cbd82833b0ae305ed96281b634e8e7e4a
Amri Karisma <amrikarisma@yahoo.com>
2021-03-19 23:26:29 +0700

    fix home url & profile url

--- resources/views/vendor/adminlte/partials/navbar/menu-item-dropdown-user-menu.blade.php
+++ resources/views/vendor/adminlte/partials/navbar/menu-item-dropdown-user-menu.blade.php
@@ -2,7 +2,7 @@
 @php( $profile_url = View::getSection('profile_url') ?? config('adminlte.profile_url', 'logout') )
 
 @if (config('adminlte.usermenu_profile_url', false))
-    @php( $profile_url = 'https://ui-avatars.com/api/?name='.session('name') )
+    @php( $profile_url = route('settings.index') )
 @endif
 
 @if (config('adminlte.use_route_url', false))
