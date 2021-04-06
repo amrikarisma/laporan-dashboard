5655b27667369308ab36072bc2545b8d769ae96c
Amri Karisma <amrikarisma@yahoo.com>
2021-04-07 03:06:15 +0700

    add select to history location

--- Modules/History/Http/Controllers/HistoryController.php
+++ Modules/History/Http/Controllers/HistoryController.php
@@ -2,7 +2,9 @@
 
 namespace Modules\History\Http\Controllers;
 
+use App\Exports\GPSExport;
 use App\Lib\MyHelper;
+use Carbon\Carbon;
 use Illuminate\Contracts\Support\Renderable;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
@@ -28,63 +30,11 @@ class HistoryController extends Controller
 
     }
 
-    /**
-     * Show the form for creating a new resource.
-     * @return Renderable
-     */
-    public function create()
+    public function downloadExcel(Request $request)
     {
-        return view('history::create');
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     * @param Request $request
-     * @return Renderable
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Show the specified resource.
-     * @param int $id
-     * @return Renderable
-     */
-    public function show($id)
-    {
-        return view('history::show');
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     * @param int $id
-     * @return Renderable
-     */
-    public function edit($id)
-    {
-        return view('history::edit');
-    }
-
-    /**
-     * Update the specified resource in storage.
-     * @param Request $request
-     * @param int $id
-     * @return Renderable
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     * @param int $id
-     * @return Renderable
-     */
-    public function destroy($id)
-    {
-        //
+        $profile = MyHelper::apiGet('profile')['data'] ?? [];
+        $cabang = $profile['anggota'] != null ? str_replace(' ','-',$profile['anggota']['cabang']['name']) : 'semua-cabang';
+        $date = Carbon::now()->locale('id_ID')->isoFormat('DD-MMMM-YYYY');
+        return (new GPSExport)->download('laporan-aktifitas-gps-'.$date.'-'.$cabang.'.xlsx');
     }
 }
