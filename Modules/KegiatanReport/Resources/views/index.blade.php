1c7ead0f67a955045a812914a2e015b39b103625
Amri Karisma <amrikarisma@yahoo.com>
2021-05-01 12:29:58 +0700

    update text export button

--- Modules/KegiatanReport/Resources/views/index.blade.php
+++ Modules/KegiatanReport/Resources/views/index.blade.php
@@ -32,10 +32,10 @@
                         <button type="submit" class="btn btn-primary">Filter</button>
                     </div>
                     <div class="col-md-auto">
-                        <a href="{{ route('laporan.kegiatan.export') }}" class="btn btn-success">Export Semua</a>
+                        <a href="{{ route('laporan.kegiatan.export') }}" class="btn btn-success">Export Laporan</a>
                     </div>
                     <div class="col-md-auto">
-                        <a href="{{ route('laporan.kegiatan.export') .'?simple=1' }}" class="btn btn-success">Export Simple</a>
+                        <a href="{{ route('laporan.kegiatan.export') .'?simple=1' }}" class="btn btn-success">Export Resume</a>
                     </div>
                 </div>
             </form>
