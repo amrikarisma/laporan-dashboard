6d380eb6cf4aa0502cfa4712f5a39e0f566c0426
Amri Karisma <amrikarisma@yahoo.com>
2021-08-03 00:49:37 +0700

    add privacy policy

--- Modules/Dashboard/Http/Controllers/DashboardController.php
+++ Modules/Dashboard/Http/Controllers/DashboardController.php
@@ -16,6 +16,7 @@ class DashboardController extends Controller
     public function index()
     {
         $profile = MyHelper::apiGet('profile')['data'] ?? [];
+
         return view('dashboard::index', compact('profile'));
     }
 
